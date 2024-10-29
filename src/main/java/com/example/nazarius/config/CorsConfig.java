059bfef7e8acff2db469f32b0b27430139216b4e
nazariusPr <protsnazar2004@gmail.com>
2024-10-29 20:30:35 +0200

    Create CorsConfig.java

---
+++ src/main/java/com/example/nazarius/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.example.nazarius.config;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOriginPatterns(List.of("http://localhost:9091"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+    config.setAllowedHeaders(
+        Arrays.asList(
+            "Access-Control-Allow-Origin",
+            "Access-Control-Allow-Headers",
+            "X-Requested-With",
+            "Origin",
+            "Content-Type",
+            "Accept",
+            "Authorization"));
+    config.setAllowCredentials(true);
+    config.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}

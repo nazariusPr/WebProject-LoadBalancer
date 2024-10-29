c08ffe217842c9005ad5b4e34da457c4f9234fbb
nazariusPr <protsnazar2004@gmail.com>
2024-10-29 22:44:57 +0200

    Update LoadBalancerConfig.java

--- src/main/java/com/example/nazarius/config/LoadBalancerConfig.java
+++ src/main/java/com/example/nazarius/config/LoadBalancerConfig.java
@@ -2,6 +2,7 @@ package com.example.nazarius.config;
 
 import com.example.nazarius.service.BalancingAlgorithmService;
 import com.example.nazarius.service.impl.RoundRobinImpl;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -11,8 +12,6 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Slf4j
 @Configuration
@@ -45,4 +44,10 @@ public class LoadBalancerConfig {
   public BalancingAlgorithmService balancingAlgorithmService() {
     return new RoundRobinImpl(List.of("http://localhost:8081"));
   }
+
+  /*
+  @Bean BalancingAlgorithmService balancingAlgorithmService(){
+      return new RandomSelectionImpl(List.of("http://localhost:8081"));
+  }
+  */
 }
